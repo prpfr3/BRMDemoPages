ed0f7ff6c2b6b8c5779562711c018fe25f11af24
paul.frost <paul.frost@talktalk.net>
2024-01-12 10:29:58 +0000

    Museum Highlights Photo Update

--- museum_highlights/script.js
+++ museum_highlights/script.js
@@ -1,40 +1,44 @@
-const body = document.body
-const slides = document.querySelectorAll('.slide')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
 
-let activeSlide = 0
+let activeSlide = 0;
+let activeImageSrc = '';
 
 rightBtn.addEventListener('click', () => {
-  activeSlide++
+  activeSlide++;
 
   if (activeSlide > slides.length - 1) {
-    activeSlide = 0
+    activeSlide = 0;
   }
 
-  setBgToBody()
-  setActiveSlide()
-})
+  setActiveSlide();
+});
 
 leftBtn.addEventListener('click', () => {
-  activeSlide--
+  activeSlide--;
 
   if (activeSlide < 0) {
-    activeSlide = slides.length - 1
+    activeSlide = slides.length - 1;
   }
 
-  setBgToBody()
-  setActiveSlide()
-})
-
-setBgToBody()
-
-function setBgToBody() {
-  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
-}
+  setActiveSlide();
+});
 
 function setActiveSlide() {
-  slides.forEach((slide) => slide.classList.remove('active'))
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+    slide.querySelector('.slide-image').classList.remove('active-image');
+  });
 
-  slides[activeSlide].classList.add('active')
+  const activeSlideElement = slides[activeSlide];
+  activeSlideElement.classList.add('active');
+  activeSlideElement
+    .querySelector('.slide-image')
+    .classList.add('active-image');
+
+  activeImageSrc = activeSlideElement
+    .querySelector('.slide-image img')
+    .getAttribute('src');
 }
